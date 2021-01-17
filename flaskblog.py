9c4638d12b2c8ad6194a239d49886df68eb0e40f
global <58117457+Blythe-hub@users.noreply.github.com>
2021-01-16 18:05:10 -0800

    data

--- flaskblog.py
+++ flaskblog.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SECRET_KEY'] = '429dca63479334f21e27012c55573cb7'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
